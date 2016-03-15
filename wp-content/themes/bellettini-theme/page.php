a69a33580ded0853b22cac5e37cbdd03d702f799
Alex Mattingley <alexmattingely@gmail.com>
2016-03-15 11:51:35 -0700

    dynamically pulling page title

--- wp-content/themes/bellettini-theme/page.php
+++ wp-content/themes/bellettini-theme/page.php
@@ -1,67 +1,84 @@
 <?php get_header(); ?>
-	<?php echo "page.php!" ?>
-			<div id="content">
-
-				<div id="inner-content" class="wrap cf">
-
-						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-
-								<header class="article-header">
-
-									<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
-
-									<p class="byline vcard">
-										<?php printf( __( 'Posted', 'bonestheme').' <time class="updated" datetime="%1$s" itemprop="datePublished">%2$s</time> '.__( 'by',  'bonestheme').' <span class="author">%3$s</span>', get_the_time('Y-m-j'), get_the_time(get_option('date_format')), get_the_author_link( get_the_author_meta( 'ID' ) )); ?>
-									</p>
-
-								</header> <?php // end article header ?>
-
-								<section class="entry-content cf" itemprop="articleBody">
-									<?php
-										// the content (pretty self explanatory huh)
-										the_content();
-
-										/*
-										 * Link Pages is used in case you have posts that are set to break into
-										 * multiple pages. You can remove this if you don't plan on doing that.
-										 *
-										 * Also, breaking content up into multiple pages is a horrible experience,
-										 * so don't do it. While there are SOME edge cases where this is useful, it's
-										 * mostly used for people to get more ad views. It's up to you but if you want
-										 * to do it, you're wrong and I hate you. (Ok, I still love you but just not as much)
-										 *
-										 * http://gizmodo.com/5841121/google-wants-to-help-you-avoid-stupid-annoying-multiple-page-articles
-										 *
-										*/
-										wp_link_pages( array(
-											'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'bonestheme' ) . '</span>',
-											'after'       => '</div>',
-											'link_before' => '<span>',
-											'link_after'  => '</span>',
-										) );
-									?>
-								</section> <?php // end article section ?>
-
-								<footer class="article-footer cf">
-
-								</footer>
-
-								<?php comments_template(); ?>
-
-							</article>
-
-							<?php endwhile; endif; ?>
-
-						</main>
-
-						<?php get_sidebar(); ?>
-
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<div id="content">
+					 <div class="container">
+						<div class="row">
+							<div id="image-responsive" class="visible-xs">
+							 	<img src="/uploads/neighborhood767.jpg"  alt="The Bellettini neighborhood">		 
+							</div>
+							<div class="col-sm-offset-2 col-sm-10">
+							 	<h1>
+							 	<?php 
+							 		if(get_field('page_header')){
+							 			echo get_field('page_header');
+							 		}else {
+							 			echo get_the_title();
+							 		} 
+							 	?>
+							 	</h1>
+							</div>
+							<div class="col-sm-2 hidden-xs" id="left-nav" style="clear:both;">		
+								<h2 class="active"><a href="/neighborhood">Neighborhood</a></h2>
+									<ul>
+										<li class=""><a href="/neighborhood/vicinity-map">Vicinity Map</a>
+										<li class=""><a href="/neighborhood/local-attractions">Local Attractions</a></li>
+									</ul>
+							</div>
+							 <div id="content-text" class="col-sm-6">
+									<?php the_content(); ?>
+							</div>
+				      <div id="contactwl" class="visible-xs">
+				<script type='text/javascript' src='http://www.westliving.net/wp-content/themes/duena/js/jquery.wl-formbuilder.js?ver=1.1'></script>
+				<script>
+				jQuery(document).ready(function($){
+					$('body').wl_formBuilder({
+						formTitle: 'Contact Us',
+						fields: {
+							1: 'name',
+							2: 'email',
+							3: 'phone',
+							4: 'message',
+						},
+						submitText: 'Submit',
+						thankyouPage: '/contact/thank-you'
+					});
+				});
+				</script>
+				<div class="well">
+				  <h2>Get More Information</h2>
+				  <div class="instrux">Please complete the form below.<br />Mandatory fields marked *</div>
+				  <div class="formbuilder-container"></div>
 				</div>
+				      
+				      </div>
+						 <div class="col-sm-4 hidden-xs">
+							<img src="/uploads/neighborhood300.jpg"  alt="The Bellettini neighborhood">			<script type='text/javascript' src='http://www.westliving.net/wp-content/themes/duena/js/jquery.wl-formbuilder.js?ver=1.1'></script>
+				<script>
+				jQuery(document).ready(function($){
+					$('body').wl_formBuilder({
+						formTitle: 'Contact Us',
+						fields: {
+							1: 'name',
+							2: 'email',
+							3: 'phone',
+							4: 'message',
+						},
+						submitText: 'Submit',
+						thankyouPage: '/contact/thank-you'
+					});
+				});
+				</script>
+				<div class="well">
+				  <h2>Get More Information</h2>
+				  <div class="instrux">Please complete the form below.<br />Mandatory fields marked *</div>
+				  <div class="formbuilder-container"></div>
+				</div>
+						 </div>
 
-			</div>
+						</div>
+					 </div>
+		</div>
+			<?php endwhile; else: ?>
+			<?php endif; ?>	
 
 <?php get_footer(); ?>
