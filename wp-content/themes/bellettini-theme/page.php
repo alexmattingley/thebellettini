32da34c4e5dbfae249c4d94cdea5d13febdbeab7
Alex Mattingley <alexmattingely@gmail.com>
2016-03-15 12:03:34 -0700

    two different contact forms...that do the exact same thing

--- wp-content/themes/bellettini-theme/page.php
+++ wp-content/themes/bellettini-theme/page.php
@@ -18,56 +18,17 @@
 							 	</h1>
 							</div>
 							<div class="col-sm-2 hidden-xs" id="left-nav" style="clear:both;">		
-								<h2 class="active"><a href="/neighborhood">Neighborhood</a></h2>
-									<ul>
-										<li class=""><a href="/neighborhood/vicinity-map">Vicinity Map</a>
-										<li class=""><a href="/neighborhood/local-attractions">Local Attractions</a></li>
-									</ul>
+								<h2><?php echo 'special navs will go here'; ?></h2>
 							</div>
 							 <div id="content-text" class="col-sm-6">
 									<?php the_content(); ?>
 							</div>
 				      <div id="contactwl" class="visible-xs">
-				<script type='text/javascript' src='http://www.westliving.net/wp-content/themes/duena/js/jquery.wl-formbuilder.js?ver=1.1'></script>
-				<script>
-				jQuery(document).ready(function($){
-					$('body').wl_formBuilder({
-						formTitle: 'Contact Us',
-						fields: {
-							1: 'name',
-							2: 'email',
-							3: 'phone',
-							4: 'message',
-						},
-						submitText: 'Submit',
-						thankyouPage: '/contact/thank-you'
-					});
-				});
-				</script>
-				<div class="well">
-				  <h2>Get More Information</h2>
-				  <div class="instrux">Please complete the form below.<br />Mandatory fields marked *</div>
-				  <div class="formbuilder-container"></div>
-				</div>
+				
 				      
 				      </div>
 						 <div class="col-sm-4 hidden-xs">
-							<img src="/uploads/neighborhood300.jpg"  alt="The Bellettini neighborhood">			<script type='text/javascript' src='http://www.westliving.net/wp-content/themes/duena/js/jquery.wl-formbuilder.js?ver=1.1'></script>
-				<script>
-				jQuery(document).ready(function($){
-					$('body').wl_formBuilder({
-						formTitle: 'Contact Us',
-						fields: {
-							1: 'name',
-							2: 'email',
-							3: 'phone',
-							4: 'message',
-						},
-						submitText: 'Submit',
-						thankyouPage: '/contact/thank-you'
-					});
-				});
-				</script>
+							<img src="/uploads/neighborhood300.jpg"  alt="The Bellettini neighborhood">			
 				<div class="well">
 				  <h2>Get More Information</h2>
 				  <div class="instrux">Please complete the form below.<br />Mandatory fields marked *</div>
@@ -80,5 +41,20 @@
 		</div>
 			<?php endwhile; else: ?>
 			<?php endif; ?>	
-
+<script type='text/javascript' src='http://www.westliving.net/wp-content/themes/duena/js/jquery.wl-formbuilder.js?ver=1.1'></script>
+				<script>
+				jQuery(document).ready(function($){
+					$('body').wl_formBuilder({
+						formTitle: 'Contact Us',
+						fields: {
+							1: 'name',
+							2: 'email',
+							3: 'phone',
+							4: 'message',
+						},
+						submitText: 'Submit',
+						thankyouPage: '/contact/thank-you'
+					});
+				});
+				</script>
 <?php get_footer(); ?>
